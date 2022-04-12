cce6d06a6c672d3bacb4e9e441ef77f18d71e8fa
Loïc Bertin <bertin.loic01@gmail.com>
2022-04-13 00:24:58 +0200

    add migration manager + makefile new commands

---
+++ SERVER/config/migrations.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+return [
+    'paths' => [
+        'migrations' => database_path('migrations'),
+        'seeds' => database_path('seeders'),
+    ],
+    'environments' => [
+        'default_migration_table' => 'phinxlog',
+        'default_database' => 'slim',
+        'slim' => [
+            'adapter' => 'mysql',
+            'host' => env('DB_HOST'),
+            'name' => env('DB_NAME'),
+            'user' => env('DB_USERNAME'),
+            'pass' => env('DB_PASSWORD'),
+            'charset' => 'utf8mb4',
+        ]
+    ]
+];
