954c4b03093219315246eaa5757231056064d19e
Loïc Bertin <bertin.loic01@gmail.com>
2022-06-02 16:57:59 +0200

    upload true CreateUser migration

--- SERVER/database/migrations/20220412150410_make_users_table_migration.php
+++ SERVER/database/migrations/20220412150410_make_users_table_migration.php
@@ -1,5 +1,6 @@
 <?php
 
+use Phinx\Db\Adapter\MysqlAdapter;
 use Phinx\Migration\AbstractMigration;
 
 class MakeUsersTableMigration extends AbstractMigration
@@ -35,8 +36,21 @@ class MakeUsersTableMigration extends AbstractMigration
 
         $table->addColumn('first_name', 'string', ['limit' => 128])
             ->addColumn('last_name', 'string', ['limit' => 128])
+            ->addColumn('username', 'string', ['limit' => 128])
             ->addColumn('email', 'string', ['limit' => 128])
             ->addColumn('password', 'string', ['limit' => 128])
+            ->addColumn('confirmed', 'boolean', ['default' => false])
+            ->addColumn('completed', 'boolean', ['default' => false])
+            ->addColumn('gender', 'enum', ['null' => true, 'values' => ['male', 'female']])
+            ->addColumn('orientation', 'enum', ['null' => true, 'values' => ['heterosexual', 'homosexual', 'bisexual']])
+            ->addColumn('bio', 'string', ['null' => true, 'limit' => 1000])
+            ->addColumn('interests', 'json', ['null' => true])
+            ->addColumn('picture', 'blob', ['null' => true, 'limit' => MysqlAdapter::BLOB_LONG])
+            ->addColumn('localisation', 'string', ['null' => true, 'limit' => 128])
+            ->addColumn('matchs', 'json', ['null' => true])
+            ->addColumn('score', 'integer', ['null' => true])
+            ->addColumn('notifications', 'json', ['null' => true])
+            ->addColumn('birthdate', 'date')
             ->addTimestamps()
             ->create();
     }
